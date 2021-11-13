5a86ceca5b1dd22420c81f7a13876e0a578bda98
Blaster4385 <venkateshchaturvedi12@gmail.com>
2021-11-13 17:40:56 +0530

    sdm710-common: Go Enforcing

--- sdm710.mk
+++ sdm710.mk
@@ -356,7 +356,8 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-service \
-    android.hardware.power@1.0-impl
+    android.hardware.power@1.0-impl \
+    android.hardware.power.stats@1.0-service.mock
 
 # Protobuf
 PRODUCT_PACKAGES += \
